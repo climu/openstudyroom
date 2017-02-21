5287bc21fd3ced691f8e9169995af0bf2f714acd
climu <climmu@gmail.com>
2017-02-21 11:12:54 +0100

    update local.py and league/urls.py for local deployment

--- openstudyroom/settings/local.py
+++ openstudyroom/settings/local.py
@@ -253,38 +253,6 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 #CSRF_COOKIE_HTTPONLY = True
 #SESSION_COOKIE_SECURE = True
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
-}
 
 CRON_CLASSES = [
 	'league.cron.ScraperCronJob',
