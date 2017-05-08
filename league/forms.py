5d02985cc1eb344cabae0a712f21c4bcfba04e8f
climu <climmu@gmail.com>
2017-05-08 18:00:39 +0200

    fix 2 small bugs

--- league/forms.py
+++ league/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import  Group
-from .models import User,Division,LeagueEvent
+from .models import User,Division,LeagueEvent,Profile
 from django.forms import ModelForm
 
 class SgfAdminForm(forms.Form):
