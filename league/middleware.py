1285f26f1ad7255cba43657b5b67314f514d1bbe
climu <climmu@gmail.com>
2017-05-15 21:05:02 +0200

    deal with loged user without timezone

--- league/middleware.py
+++ league/middleware.py
@@ -6,7 +6,7 @@ import pytz
 class TimezoneMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
-        if request.user.is_authenticated():
+        if request.user.is_authenticated() and request.user.profile.timezone is not None:
             timezone.activate(pytz.timezone(request.user.profile.timezone))
         else:
             timezone.deactivate()
