8d5a8db3c24eee0134ba7dd7b891362df127d495
climu <climmu@gmail.com>
2017-05-31 10:08:24 +0200

    user can select when available

--- fullcalendar/views.py
+++ fullcalendar/views.py
@@ -9,7 +9,7 @@ import json
 from django.utils.timezone import make_aware
 from league.models import is_league_admin, is_league_member
 from .forms import UTCPublicEventForm
-from .models import PublicEvent
+from .models import PublicEvent,AvailableEvent
 from pytz import utc,timezone
 #from django.utils import timezone
 # Create your views here.
@@ -52,25 +52,43 @@ def calendar_view(request):
 def json_feed(request):
 	'''get all events for one user and serve a json'''
 	user = request.user
-	tz=request.user.get_timezone()
-#	if user.is_authenticated():
-	#	me_available_events = CalEvent.objects.filter(type='available',users = user)
-	#	divisions = user.get_open_divisions()
-
+	if user.is_authenticated :
+		tz=user.get_timezone()
+	else:
+		tz = utc
+	# get public events for everyone
 	public_events = PublicEvent.objects.all()
 	data = []
 	for event in public_events:
 		dict={
-		'id' : event.pk,
+		'id' : 'public:' + str(event.pk),
 		'title' : event.title,
 		'start' : event.start.astimezone(tz).strftime('%Y-%m-%d %H:%M:%S'),
-		'end' : event.end.astimezone(tz).strftime('%Y-%m-%d  %H:%M:%S'),
+		'end' : event.end.astimezone(tz).strftime('%Y-%m-%d %H:%M:%S'),
 		'is_new': False,
 		'editable': False,
+		'type':'public',
 		}
 		if event.url:
 			dict['url'] = event.url
 		data.append(dict)
+	# get user related available events
+	if user.is_authenticated() and user.user_is_league_member():
+		me_available_events = AvailableEvent.objects.filter(user=user)
+		for event in me_available_events:
+			dict={
+			'id' : 'me-a:' + str(event.pk),
+			'title' : 'I am available',
+			'start' : event.start.astimezone(tz).strftime('%Y-%m-%d %H:%M:%S'),
+			'end' : event.end.astimezone(tz).strftime('%Y-%m-%d %H:%M:%S'),
+			'is_new': False,
+			'editable': True,
+			'type':'me-available',
+			'color': '#01DF3A',
+        	'className' :'me-available',
+			}
+
+			data.append(dict)
 	return HttpResponse(json.dumps(data), content_type = "application/json")
 
 
@@ -80,19 +98,47 @@ def json_feed(request):
 @user_passes_test(is_league_member,login_url="/",redirect_field_name = None)
 def save(request):
 	'''get events modification from calendar ajax post'''
-	tz=request.user.get_timezone()
+	user = request.user
+	tz=user.get_timezone()
 	if request.method == 'POST':
+		changed_events = ''
 		changed_events = json.loads(request.POST.get('events'));
+		print(changed_events)
 		for event in changed_events:
-			start = datetime.strptime(event['start'],'%Y-%m-%dT%H:%M:%S')
-			start= make_aware(start,tz)
-			end = datetime.strptime(event['end'],'%Y-%m-%dT%H:%M:%S')
-			end= make_aware(end,tz)
-			if event['is_new']:#we create a new event on server
-				#if event['type'] == 'me-available':
+			print(event)
+			if event['type'] == 'deleted':#we deleted an event
+				#event pk is stored in event title type:pk
+				a = event['title'].find(':')
+				pk = event['title'][a+1:]
+				if event['title'].startswith('me-a'):
+					#we had user=user to be sure one user can only delete his available events
+					ev = get_object_or_404(AvailableEvent,user=user,pk=pk)
+					ev.delete()
 
+			elif event['is_new']:#we create a new event on server
+				start = datetime.strptime(event['start'],'%Y-%m-%dT%H:%M:%S')
+				start= make_aware(start,tz)
+				end = datetime.strptime(event['end'],'%Y-%m-%dT%H:%M:%S')
+				end= make_aware(end,tz)
+				print(end)
+				if event['type'] == 'me-available':
+					new_event = AvailableEvent.objects.create(start =start,end=end,user=user)
 
-				return HttpResponse('success')
+			else:#the event must have been moved or resized.
+				start = datetime.strptime(event['start'],'%Y-%m-%dT%H:%M:%S')
+				start= make_aware(start,tz)
+				end = datetime.strptime(event['end'],'%Y-%m-%dT%H:%M:%S')
+				end= make_aware(end,tz)
+				a = event['id'].find(':')
+				pk = event['id'][a+1:]
+				if event['id'].startswith('me-a'):
+					ev = get_object_or_404(AvailableEvent,user=user,pk=pk)
+
+				ev.start = start
+				ev.end = end
+				ev.save()
+
+	return HttpResponse('success')
 
 
 @login_required()
@@ -105,7 +151,7 @@ def admin_cal_event_list(request):
 @user_passes_test(is_league_admin,login_url="/",redirect_field_name = None)
 def admin_delete_event(request,pk):
 	if request.method == 'POST':
-		event = get_object_or_404(pk=pk)
+		event = get_object_or_404(PublicEvent,pk=pk)
 		event.delete()
 	else:
 		raise Http404("What are you doing here ?")
