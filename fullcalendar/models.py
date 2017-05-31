6dfb8b6d19b9709a8679a08a6249be67443c1c50
climu <climmu@gmail.com>
2017-05-31 10:11:41 +0200

    Revert "Game planing tool"

--- fullcalendar/models.py
+++ fullcalendar/models.py
@@ -9,33 +9,44 @@ from league.models import User,Division
 # Create your models here.
 
 class CalEvent(models.Model):
-
-	start = models.DateTimeField()
-	end =  models.DateTimeField()
-	class Meta:
-		abstract = True
-
-class PublicEvent(CalEvent):
+	EVENT_TYPE_CHOICES = (
+        ('public', 'public'),
+        ('personal', 'personal'),
+        ('division', 'division'),
+        )
+	begin_time = models.DateTimeField()
+	end_time =  models.DateTimeField()
 	title = models.CharField(max_length=20)
 	description = models.TextField(blank=True)
 	url = models.URLField(blank=True)
+	type = models.CharField(# public, personal, division
+		max_length=10,
+		choices=EVENT_TYPE_CHOICES,
+		default='public')
+	users = models.ManyToManyField(User, blank=True) # games appointment are related to 2 users
+	division = models.ManyToManyField(Division, blank=True) # availability to play is related to a division
 
 	@staticmethod
-	def get_future_public_events():
-		'''return a query of all future public events to a user.'''
+	def get_cal_events(user):
+		'''return a query of all event related to a user.'''
+		if user.is_authenticated and user.user_is_league_member:
+			divisions = user.get_open_divisions()
+			cal_events = CalEvent.objects.filter(Q(type= 'public')|Q(type = 'personal',users = user)|Q(type = 'division',division__in = divisions))
+			#personal_events = CalEvent.objects.filter(type = 'personal',users = user)
+			#division_events = CalEvent.objects.filter(type ='division,' division = divisions )
+		else :
+			cal_events = CalEvent.objects.filter(type= 'public')
+		return cal_events
+
+	@staticmethod
+	def get_future_cal_events(user):
+		'''return a query of all future event related to a user.'''
 		now = timezone.now()
-		public_events = PublicEvent.objects.filter(end__gte = now)
-		return public_events
-
-class AvailableEvent(CalEvent):
-	user = models.ForeignKey(User)
-
-class GameRequestEvent(CalEvent):
-	sender = models.ForeignKey(User,related_name="%(app_label)s_%(class)s_related_sender",
-        related_query_name="%(app_label)s_%(class)ss_sender",)
-	receivers = models.ManyToManyField(User,related_name="%(app_label)s_%(class)s_related_receiver",
-        related_query_name="%(app_label)s_%(class)ss_receiver",)
-
-class GameAppointmentEvent(CalEvent):
-	users = models.ManyToManyField(User,related_name="%(app_label)s_%(class)s_related",
-        related_query_name="%(app_label)s_%(class)ss",)
+		if user.is_authenticated and user.user_is_league_member:
+			divisions = user.get_open_divisions()
+			cal_events = CalEvent.objects.filter(end_time__gte = now).filter(Q(type= 'public')|Q(type = 'personal',users = user)|Q(type = 'division',division__in = divisions))
+			#personal_events = CalEvent.objects.filter(type = 'personal',users = user)
+			#division_events = CalEvent.objects.filter(type ='division,' division = divisions )
+		else :
+			cal_events = CalEvent.objects.filter(type= 'public').filter(end_time__gte = now)
+		return cal_events
