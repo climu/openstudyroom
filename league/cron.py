7a4da83727661920834825277466bb078b2b8fcc
climu <climmu@gmail.com>
2017-06-10 15:27:13 +0200

    again, git mess

--- league/cron.py
+++ league/cron.py
@@ -1,12 +1,11 @@
 from django_cron import CronJobBase, Schedule
 from .views import scraper
 
+
 class ScraperCronJob(CronJobBase):
-	RUN_EVERY_MINS = 5 # every 5 mins
-	schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-	code = 'league.scraper_cron_job'    # a unique code
-	def do(self):
-		scraper()
+    RUN_EVERY_MINS = 5  # every 5 mins
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code = 'league.scraper_cron_job'    # a unique code
 
-
-
+    def do(self):
+        scraper()
