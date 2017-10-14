c826660f03fd9cf0e9a380a31cb25095d9e4149b
Nutler <mildchestnut@gmail.com>
2017-10-14 23:39:21 +0300

    Return 0 if username is not provided to get_user_id in ogs.py

--- league/ogs.py
+++ league/ogs.py
@@ -6,6 +6,8 @@ from .models import Sgf
 def get_user_id(username):
     """Test if a username is registered in ogs and return his id if so.
     Otherwise we return 0"""
+    if not username:
+        return 0
     url = 'https://online-go.com/api/v1/players/?username=' + username
     request = requests.get(url).json()
     # Test if usernmae exists at OGS.
