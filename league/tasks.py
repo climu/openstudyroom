35ec3e1d8ce2d59a2b9433486629f14d4eab9c8e
climu <climmu@gmail.com>
2017-10-31 14:15:08 +0100

    kgs_check_online works

--- league/tasks.py
+++ league/tasks.py
@@ -1,6 +1,15 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from django.conf import settings
+from django.utils import timezone
+
+from .models import Profile
+import requests
+import json
+
+def kgs_update_online():
+    chain = kgs_connect.s() | kgs_update_online_with_cookies.s()
+    chain()
 
 
 @shared_task(bind=True, retry_backoff=True, max_retries=10)
@@ -8,11 +17,12 @@ def kgs_connect(self):
     """Connect to KGS server and retunr cookie.
         Could we consider using redis memory db to store this cookie somehow?
     """
+
     url = 'http://www.gokgs.com/json/access'
-    # If you are running this locally and want to run scraper, you should use your own
+    # If you are running this locally, you should use your own
     # KGS credential
     if settings.DEBUG:
-        kgs_password = 'yourpassword' # change this for local test
+        kgs_password = 'ùypassword' # change this for local test
     else:
         with open('/etc/kgs_password.txt') as f:
             kgs_password = f.read().strip()
@@ -25,7 +35,30 @@ def kgs_connect(self):
     }
     formatted_message = json.dumps(message)
     response = requests.post(url, formatted_message)
+
     if response.status_code == 200:
         return response.cookies
     else:
         raise self.retry()
+
+
+@shared_task(bind=True, retry_backoff=True, max_retries=10)
+def kgs_update_online_with_cookies(self, cookies):
+    """Update profiles of users connected to KGS."""
+
+    url = 'http://www.gokgs.com/json/access'
+    response = requests.get(url, cookies=cookies)
+    if response.status_code == 200:
+        print('yay')
+        now = timezone.now()
+        for m in json.loads(response.text)['messages']:
+            if m['type'] == 'ROOM_JOIN' and m['channelId'] == 3627409:
+                for kgs_user in m['users']:
+                    osr_profile = Profile.objects.filter(
+                        kgs_username__iexact=kgs_user['name']).first()
+                    if osr_profile is not None:
+                        osr_profile.last_kgs_online = now
+                        osr_profile.save()
+                break
+    else:
+        self.retry()
