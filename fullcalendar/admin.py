0f1b30669d4c60a3b92dacb2cd50cdb0a1f1fb43
climu <climmu@gmail.com>
2018-02-22 18:22:23 +0100

    need game appointment in admin

--- fullcalendar/admin.py
+++ fullcalendar/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import PublicEvent, GameRequestEvent
+from .models import PublicEvent, GameRequestEvent, GameAppointmentEvent
 # Register your models here.
-mymodels = [PublicEvent, GameRequestEvent]
+mymodels = [PublicEvent, GameRequestEvent, GameAppointmentEvent]
 
 admin.site.register(mymodels)
