e71e275363c7198738ddaf9ad3c9adbd582e127c
climu <climmu@gmail.com>
2018-02-23 20:45:40 +0100

    refix kgs rank at scraper

--- league/views.py
+++ league/views.py
@@ -62,6 +62,7 @@ def scraper():
             for kgs_user in m['users']:
                 profile = Profile.objects.filter(kgs_username__iexact=kgs_user['name']).first()
                 if profile is not None:
+                    if 'rank' in kgs_user:
                     profile.kgs_rank = kgs_user['rank'] # scraping the rank of the players
                     profile.last_kgs_online = now
                     profile.save()
