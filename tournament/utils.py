abc58eb675fc174e63278c7496507f4d04e43c9f
climu <climmu@gmail.com>
2018-03-06 23:03:04 +0100

    fix save brackets

--- tournament/utils.py
+++ tournament/utils.py
@@ -9,11 +9,11 @@ def save_round(round, matches):
         match = get_object_or_404(Match, pk=match_id, round=round)
         if match.sgf is None:
             if len(players) > 0:
-                player_1 = get_object_or_404(TournamentPlayer, pk=players[0], event=round.tournament)
+                player_1 = get_object_or_404(TournamentPlayer, pk=players[0], event=round.bracket.tournament)
                 match.player_1 = player_1
                 match.player_2 = None
                 if len(players) == 2:
-                    player_2 = get_object_or_404(TournamentPlayer, pk=players[1], event=round.tournament)
+                    player_2 = get_object_or_404(TournamentPlayer, pk=players[1], event=round.bracket.tournament)
                     match.player_2 = player_2
                 if len(players) < 3:
                     match.save()
