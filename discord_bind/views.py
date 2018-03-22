df310a85f972435bb1c23d6631556998c7c5b761
climu <climmu@gmail.com>
2018-03-22 15:05:58 +0100

    remove guilds.join scope for discord

--- discord_bind/views.py
+++ discord_bind/views.py
@@ -52,8 +52,9 @@ def oauth_session(request, state=None, token=None):
     else:
         redirect_uri = request.build_absolute_uri(
             reverse('discord_bind_callback'))
-    scope = (['identify', 'email', 'guilds.join'] if settings.DISCORD_EMAIL_SCOPE
-             else ['identity', 'guilds.join'])
+    scope = (['identify', 'email'] if settings.DISCORD_EMAIL_SCOPE
+             else ['identity'])
+    # add scope 'guilds.join' for invite
     return OAuth2Session(settings.DISCORD_CLIENT_ID,
                          redirect_uri=redirect_uri,
                          scope=scope,
