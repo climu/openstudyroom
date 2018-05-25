d12839dd67438747beb80489f7d653f24a500874
czabo <cezary.zabo@gmail.com>
2018-05-25 15:55:27 +0200

    download as zip works

--- league/views.py
+++ league/views.py
@@ -182,15 +182,18 @@ def download_all_sgf(request, user_id):
     white_sgfs = user.white_sgf.get_queryset()
 
     '''dictonary of all sgfs of a specific user'''
+    '''TODO: two sfgs can have same name, union would take just one file'''
 
-    for sgf in black_sgfs.all():
+    for sgf in white_sgfs.union(black_sgfs).all():
         single_sgf = HttpResponse(sgf.sgf_text, content_type='application/octet-stream')
         single_sgf['Content-Disposition'] = 'attachment; filename="' +  \
             sgf.wplayer + '-' + sgf.bplayer + '-' + sgf.date.strftime('%m/%d/%Y') + '.sgf"'
-        zip.writestr("test1.sgf", single_sgf.content)
+        zip.writestr(sgf.wplayer + '-' + sgf.bplayer + ".sgf", single_sgf.content)
 
-    response = HttpResponse(content_type="application/zip")
-    response["Content-Disposition"] = "attachment; filename=sgfs.zip"
+    zip.close()
+
+    response = HttpResponse(content_type="application/x-zip-compressed")
+    response["Content-Disposition"] = "attachment; filename=" + user.username + ".zip"
 
     in_memory.seek(0)
     response.write(in_memory.read())
