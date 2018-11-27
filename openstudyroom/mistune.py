4f9667d49c5581edd6b0903c88b196f7d8b22523
climu <climmu@gmail.com>
2018-11-27 09:56:46 +0100

    add img-responsive class to forum img

--- openstudyroom/mistune.py
+++ openstudyroom/mistune.py
@@ -5,6 +5,22 @@ class osr_renderer(mistune.Renderer):
         """ All point here is to disable autolink feature."""
         return link
 
+    def image(self, src, title, text):
+        """Rendering a image with title and text.
+        :param src: source link of the image.
+        :param title: title text of the image.
+        :param text: alt text of the image.
+        """
+        src = mistune.escape_link(src)
+        text = mistune.escape(text, quote=True)
+        if title:
+            title = mistune.escape(title, quote=True)
+            html = '<img src="%s"  class="img-responsive" alt="%s" title="%s"' % (src, text, title)
+        else:
+            html = '<img src="%s" class="img-responsive" alt="%s"' % (src, text)
+        return '%s />' % html
+
+
 
 def osr_markdown(string, *args, **kwargs):
     renderer = osr_renderer()
