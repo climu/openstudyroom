climu/openstudyroom

commit ad598208bf57d5653b0d95cd6aabeab02003837c

count users on welcome message

league/views.py
@@ -724,7 +724,8 @@ def admin(request):
                 else:
                     with open('/etc/discord_welcome_hook_url.txt') as f:
                         discord_url = f.read().strip()
-                message = "Please welcome our new member " + user.username + " with a violent game of baduk. \n"
+                n_users = User.objects.filter(groups__name__in=['league_member']).count()
+                message = "Please welcome our " + str(n_users) + "th user **" + user.username + "** with a violent game of baduk. \n"
                 if user.profile.kgs_username:
                     message += "KGS : " + user.profile.kgs_username + " \n"
                 if user.profile.ogs_username:

commit 115d20a05575428a4e6d1110a80be8ce98fab657

fix pylint (broken by ad598208bf57d5653b0d95cd6aa)

league/views.py
@@ -725,7 +725,7 @@ def admin(request):
                     with open('/etc/discord_welcome_hook_url.txt') as f:
                         discord_url = f.read().strip()
                 n_users = User.objects.filter(groups__name__in=['league_member']).count()
-                message = "Please welcome our " + str(n_users) + "th user **" + user.username + "** with a violent game of baduk. \n"
+                message = "Please welcome our %dth user **%s** with a violent game of baduk.\n" % (n_users, user.username)
                 if user.profile.kgs_username:
                     message += "KGS : " + user.profile.kgs_username + " \n"
                 if user.profile.ogs_username: