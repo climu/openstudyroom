3e82bdff03909e4e80482aedad83a4bb662bd06a
Sajjad Heydari <mcshemail@gmail.com>
2019-01-03 23:36:28 +0330

    Removing sleep1
    https://github.com/climu/openstudyroom/pull/327\#discussion_r245110301

--- docker.sh
+++ docker.sh
@@ -5,10 +5,7 @@ export PORTS="-p 8000:8000"
 export VOLUME="-v $(pwd):/app"
 
 docker stop $NAME
-sleep 1
 docker rm $NAME
-sleep 1
 docker build -t osr/$NAME . || exit 1
-sleep 1
 docker run --restart=always --name=$NAME $PORTS $VOLUME -d osr/$NAME
 
