75079ac9c3c2c279ed301b71ddbdf0a5103726c1
MCSH <mcshemail@gmail.com>
2019-01-03 22:46:12 +0330

    Updated run.sh to use set-eu

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -eu
+
 INITI_DATA= [ ! -f db.sqlite3 ]
 
-./manage.py makemigrations || exit 1
-./manage.py migrate || exit 1
+./manage.py makemigrations
+./manage.py migrate
 
 if $INIT_DATA ; then
-    ./manage.py loaddata fixtures/initial_data.json || exit 1
+    ./manage.py loaddata fixtures/initial_data.json
 fi
 
-./manage.py runserver 0.0.0.0:8000 || exit 1
+./manage.py runserver 0.0.0.0:8000
