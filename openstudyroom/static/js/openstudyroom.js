6a57836a7ca687c991283f77b64a9f630eaf6b8c
climu <climmu@gmail.com>
2019-03-30 11:55:12 +0100

    debounce

--- openstudyroom/static/js/openstudyroom.js
+++ openstudyroom/static/js/openstudyroom.js
@@ -48,7 +48,7 @@ function togglesearch(){
   $('#search-button').toggleClass('text-primary')
 }
 
-function autocomplet() {
+var autocomplet = debounce(function() {
 	var min_length = 2;
 	var query = $('#search-input').val();
 	if (query.length >= min_length) {
@@ -57,7 +57,8 @@ function autocomplet() {
 	} else {
 		$('#search-results').hide();
 	}
-}
+},
+250)
 
 function request_pages(query){
   $.ajax({
@@ -144,3 +145,22 @@ function format_user_search(user){
     html += '">' + user['username'] +'</a>'
     return(html)
   }
+
+  // Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
