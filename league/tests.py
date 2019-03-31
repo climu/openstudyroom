9d5c55b41c8f77032d288e371ab52b9ffe0b70e3
climu <climmu@gmail.com>
2019-03-31 01:12:10 +0100

    fix parse test

--- league/tests.py
+++ league/tests.py
@@ -35,6 +35,7 @@ class TestUtils(unittest.TestCase):
             'place': 'The KGS Go Server at http://www.gokgs.com/',
             'number_moves': 268,
             'check_code': '20161119climunomenestmnhnimcf11',
+            'handicap': 0,
         }
         assert parsed == expected
 
