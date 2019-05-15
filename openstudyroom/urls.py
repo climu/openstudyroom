climu/openstudyroom

commit 5e7c33627f72e9206f36f054cb33f88172f10ede

update puput urls conf

openstudyroom/urls.py
@@ -9,6 +9,7 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from machina.app import board
+from puput import urls as puput_urls
 
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
@@ -37,7 +38,7 @@ urlpatterns = [
 
     url(r'^messages/', include('postman.urls', namespace='postman')),
     url(r'^community/', include('community.urls', namespace='community')),
-    url(r'', include('puput.urls')),
+    url(r'',include(puput_urls)),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:

commit 69057b73a5a52718ab2c1e6ae4e7d74435fa092c

fix pylint
broken in 5e7c336

openstudyroom/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
 
     url(r'^messages/', include('postman.urls', namespace='postman')),
     url(r'^community/', include('community.urls', namespace='community')),
-    url(r'',include(puput_urls)),
+    url(r'', include(puput_urls)),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
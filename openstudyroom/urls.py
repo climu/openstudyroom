69057b73a5a52718ab2c1e6ae4e7d74435fa092c
raylu <lurayl@gmail.com>
2019-05-15 13:52:47 -0700

    fix pylint
    broken in 5e7c336

--- openstudyroom/urls.py
+++ openstudyroom/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
 
     url(r'^messages/', include('postman.urls', namespace='postman')),
     url(r'^community/', include('community.urls', namespace='community')),
-    url(r'',include(puput_urls)),
+    url(r'', include(puput_urls)),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
