climu/openstudyroom

commit 52843fdffd86f1006ee42306a0cd98e696478c6f

#364 set handicap to 1 if komi is 0.5

league/utils.py
@@ -171,6 +171,9 @@ def parse_sgf_string(sgf_string):
     out['board_size'] = int(out['board_size'])
     out['handicap'] = int(out['handicap'])
 
+    # Set handicap to 1 if handicap is 0 and komi is 0.5: https://github.com/climu/openstudyroom/issues/364
+    if out['handicap'] == 0 and out['komi'] == 0.5:
+        out['handicap'] = 1
 
     # counting the number of moves. Note that there could be a +-1 diff, but we don't really care
     out['number_moves'] = 2 * sgf_string.count(';B[')

commit d7f57c1f616ad3a885971ec164f829ddc9bfc291

fix SGF test for handicap 1 case
broken in 52843fd

league/tests.py
@@ -35,7 +35,7 @@ class TestUtils(unittest.TestCase):
             'place': 'The KGS Go Server at http://www.gokgs.com/',
             'number_moves': 268,
             'check_code': '20161119climunomenestmnhnimcf11',
-            'handicap': 0,
+            'handicap': 1,
         }
         assert parsed == expected
 

league/utils.py
@@ -163,7 +163,7 @@ def parse_sgf_string(sgf_string):
             q = sgf_string.find(']', p)  # find the end of the tag
             out[prop[key]] = sgf_string[p + 3:q]
 
-    # Format  date, komi and time in proper type
+    # Format date, komi and time in proper type
     if out['date'] is not None:
         out['date'] = datetime.datetime.strptime(out['date'], "%Y-%m-%d")
     out['komi'] = float(out['komi'])