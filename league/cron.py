21834c98b9b469b3bf593747002bf55326a46205
climu <climmu@gmail.com>
2019-12-27 11:46:06 +0100

    update scraper time to 2 mins

--- league/cron.py
+++ league/cron.py
@@ -3,7 +3,7 @@ from .views import scraper
 
 
 class ScraperCronJob(CronJobBase):
-    RUN_EVERY_MINS = 5  # every 5 mins
+    RUN_EVERY_MINS = 2  # every 5 mins
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'league.scraper_cron_job'    # a unique code
 
